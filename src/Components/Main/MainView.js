896975c57324a1ee1e36ebd171cf8a6063198e09
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2022-01-29 21:38:55 +0300

    animation

--- src/Components/Main/MainView.js
+++ src/Components/Main/MainView.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React ,{useRef,useEffect} from 'react';
 import './Main.css';
+import {TweenMax} from 'gsap';
 
 function MainView(props){
+  let logoItem =useRef(null);
+  useEffect(()=>{
+      TweenMax.from(
+          logoItem,{
+              duration: 4,
+              fontSize: '0px',
+              opacity: 0,
+              ease: 'sine',
+              color:'transparent'
+          }
+      )
+  })
   
   return (
       <div className={props.className}>
 
-        <div  className='wordings'>
+        <div  ref={el=>{logoItem=el}} className='wordings'>
     
-          <h2>AMANI CARWASH</h2>
+          <h2 >AMANI CARWASH</h2>
           <h6>{props.title2}</h6>
 
      
         </div>
+
         <div className='wordings1'>
     
     <h2>AMANI CARWASH</h2>
