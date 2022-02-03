e73d39b3f7b1c7fe6e5b613e766f65c16540aa0a
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2022-02-03 23:48:21 +0300

    ...

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,20 +1,40 @@
 import { Button } from 'bootstrap';
-import React from 'react';
+import React,{useState} from 'react';
 import Googlemap from './Googlemap';
 import './Map.css';
 
 function Map() {
+  const [input,setInput]= useState('');
+  const [textInput,setTextInput]=useState('');
+
+  const onChange =(e)=>{
+   setInput(e.target.value)
+  }
+  const onTextChange =(e)=>{
+    setTextInput(e.target.value)
+   }
+  const onhandleSubmit =(e)=>{
+    e.preventDefault()
+    setInput('')
+    setTextInput('')
+   }
   return (
       <div className='Map'>
-<form  className='form'>
+<form  onSubmit={onhandleSubmit} className='form'>
     <input 
     style={{padding:'5px 33px', borderRadius:'5px', border:'none'}}
     className='form-control'
     placeholder='email address'
     type='text'
+    value={input}
+    onChange={onChange}
     />
     <br/>
-    <textarea placeholder='message' style={{ margin:'2rem',borderRadius:'5px', border:'none'}} className='form-control' cols='40' rows='9'>
+    <textarea placeholder='message'
+    value={textInput}
+    onChange={onTextChange}
+     style={{ margin:'2rem',borderRadius:'5px', border:'none'}}
+     className='form-control' cols='40' rows='9'>
 
     </textarea>
     <br />
